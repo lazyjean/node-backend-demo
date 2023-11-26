57aaeb9a8d25034e2e6920fb6dba01b04c9efba4
liuzhen <lazyjean@foxmail.com>
2023-11-26 22:08:08 +0800

    improve: improve lock account logic when attempts too many login

--- src/services/user/repository/redis.ts
+++ src/services/user/repository/redis.ts
@@ -1,42 +1,36 @@
-import { createClient } from 'redis';
+import {createClient} from 'redis';
 import {logger} from "../../../app/logger";
 
 const AuthLoginAttemptsCntKeyPrefix = "auth:login:attempts-cnt:"
 
 const client = createClient({
-  url: process.env.REDIS_HOST || "localhost:6379",
+    url: process.env.REDIS_HOST || "localhost:6379",
 });
 
 (async () => {
-  await client.on('error', (err) => {
-    logger.error(err);
-  }).on('connect', () => {
-    logger.info('Redis connected');
-  }).connect();
+    await client.on('error', (err) => {
+        logger.error(err);
+    }).on('connect', () => {
+        logger.info('Redis connected');
+    }).connect();
 })();
 
 export const increaseLoginFailedCount = async (username: string) => {
-  const key = AuthLoginAttemptsCntKeyPrefix + username
-  var setExpire = false
-  if (!await client.exists(key)) {
-    setExpire = true
-  }
-  await client.incr(AuthLoginAttemptsCntKeyPrefix + username)
-  if (setExpire) {
-    await client.expire(AuthLoginAttemptsCntKeyPrefix + username, 5 * 60)
-  }
+    const key = AuthLoginAttemptsCntKeyPrefix + username
+    await client.incr(key)
+    await client.expire(key, 5 * 60)
 };
 
 export const getLoginFailedCount = async (username: string) => {
-  const str = await client.get(AuthLoginAttemptsCntKeyPrefix + username)
-  return str? parseInt(str) : 0
+    const str = await client.get(AuthLoginAttemptsCntKeyPrefix + username)
+    return str ? parseInt(str) : 0
 };
 
 export const resetLoginFailedCount = async (username: string) => {
-  await client.del(AuthLoginAttemptsCntKeyPrefix + username)
+    await client.del(AuthLoginAttemptsCntKeyPrefix + username)
 };
 
 export default {
-  increaseLoginFailedCount,
-  getLoginFailedCount
+    increaseLoginFailedCount,
+    getLoginFailedCount
 }
